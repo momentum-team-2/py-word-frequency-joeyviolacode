6bb236a317ff5efeae5d8b86ad79acbc87dbafbe
joeyviolacode <joeyviolacode@gmail.com>
2020-06-16 15:07:45 -0400

    Initial commit.  File-processing and text-stripping functional

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import re
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -7,7 +9,24 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    this_file = open(file)
+    text = this_file.readlines()
+    word_list = process_text_to_words(text)
+    print(word_list)
+
+def process_text_to_words(text):
+    str = ""
+    for item in text:
+        str += item
+    words = re.sub(r'\[\s-\s|"|,\s|,|\.\n|\.\s|\s|\n|\.|:\]', "*", str)
+    words2 = words.split("*")
+    words_filtered = [word for word in words2 if len(word) > 1 and word not in STOP_WORDS]
+    words_filtered.sort()
+    return words_filtered
+
+
+
+
 
 
 if __name__ == "__main__":
