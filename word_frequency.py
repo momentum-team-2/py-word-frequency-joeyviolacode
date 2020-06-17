bebb5f94372f050d8a8ec11c8bcdf281d5912a10
joeyviolacode <joeyviolacode@gmail.com>
2020-06-16 21:12:37 -0400

    Fully functional, needs better regex so length check unnecessary.

--- word_frequency.py
+++ word_frequency.py
@@ -6,27 +6,45 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
-
-def print_word_freq(file):
+def controller(file):
     """Read in `file` and print out the frequency of words in that file."""
     this_file = open(file)
     text = this_file.readlines()
+
     word_list = process_text_to_words(text)
-    print(word_list)
+    dictionary_info = count_words(word_list)
+    print_word_freq(dictionary_info[0], dictionary_info[1])
+    this_file.close()
 
 def process_text_to_words(text):
     str = ""
     for item in text:
         str += item
+    str = str.lower()
     words = re.sub(r'\[\s-\s|"|,\s|,|\.\n|\.\s|\s|\n|\.|:\]', "*", str)
     words2 = words.split("*")
     words_filtered = [word for word in words2 if len(word) > 1 and word not in STOP_WORDS]
     words_filtered.sort()
     return words_filtered
 
+def count_words(list):
+    dictionary = {}
+    max_count = 1
+    for word in list:
+        if word in dictionary:
+            dictionary[word] += 1
+            if dictionary[word] > max_count:
+                max_count = dictionary[word]
+        else:
+            dictionary[word] = 1
+    return [dictionary, max_count]
 
-
-
+def print_word_freq(frequency_dictionary, max_count):
+    for word_count in range(max_count, 0, -1):
+        word_list = [k for k,v in frequency_dictionary.items() if v == word_count]
+        if len(word_list) > 0:
+            for word in word_list:
+                print(word.ljust(15) + "|  " + str(word_count).ljust(3) + " " + (word_count * "*"))
 
 
 if __name__ == "__main__":
@@ -40,7 +58,7 @@ if __name__ == "__main__":
 
     file = Path(args.file)
     if file.is_file():
-        print_word_freq(file)
+        controller(file)
     else:
         print(f"{file} does not exist!")
         exit(1)
